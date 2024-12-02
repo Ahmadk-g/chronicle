52fa4b5c952c5d39e419c4492e2ee057eb1d45b2
Ahmad Khalaf <khalaf.ahmed@hotmail.com>
2024-12-02 15:15:48 +0000

    Create 'NotFound' page

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import ProfilePage from './pages/profiles/ProfilePage';
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import NotFound from './components/NotFound';
 
 function App() {
   const currentUser = useCurrentUser();
@@ -100,7 +101,7 @@ function App() {
                 path="/profiles/:id/edit"
                 render={() => <ProfileEditForm />}
               />
-          <Route render={() => <p>Page not found!</p>} />
+          <Route render={() => <NotFound />} />
         </Switch>
       </Container>
     </div>
