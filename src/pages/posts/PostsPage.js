e947fbb7ac7c7a2be88918de38c7677be60b6c73
Ahmad Khalaf <khalaf.ahmed@hotmail.com>
2024-12-02 15:36:03 +0000

    Prepare for deployment

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -18,6 +18,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
 import { useRedirect } from "../../hooks/useRedirect";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -25,6 +26,8 @@ function PostsPage({ message, filter = "" }) {
   const { pathname } = useLocation();
 
   const [query, setQuery] = useState("");
+
+  const currentUser = useCurrentUser();
 
   // Determine the heading based on pathname or filter
   const getPageHeading = () => {
@@ -70,7 +73,7 @@ function PostsPage({ message, filter = "" }) {
     return () => {
       clearTimeout(timer);
     };
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser]);
 
   return (
     <Row className="h-100">
